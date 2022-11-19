fb55c8fb73ae7bd13f0501e2bd442f5cccd32e7e
Esamanoaz <esamanoaz@gmail.com>
2022-11-19 11:15:22 -0700

    working on the auciton gameplay some more

--- auction_game.py
+++ auction_game.py
@@ -1,5 +1,8 @@
+from random import shuffle
+
+
 def get_artifacts():
-    return ['J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'J', 'Q', 'K', 'A', 'joker', 'joker']
+    return shuffle([1, 2, 3, 4, 1, 2, 3, 4, 0]) # 1, 2, 3, 4, 0,   ==   J, Q, K, A, Joker
 
 
 def get_money():
@@ -7,22 +10,20 @@ def get_money():
 
 
 class Player:
-    def __init__(self, _money):
+    def __init__(self, _money=get_money()):
         self.money = _money
         self.artifacts_won = []
-        self.scoring = {
-            'J': 1,
-            'Q': 2,
-            'K': 3,
-            'A': 4
-        }
+        self.score = 0
+        self.bid = 0
     
 
-    def cal_score(self):
-        score = 0
-        for a in self.artifacts_won:
-            score += self.scoring[a]
-        return score
+    def add_artifact(self, artifact):
+        self.artifacts_won.append(artifact)
+        self.score += artifact
+    
+
+    def bid(self, _bid=input('What is this player\'s bid? ')):
+        self.bid = _bid
 
 
 def play(players):
@@ -30,12 +31,24 @@ def play(players):
     p_one, p_two, p_three = players
 
     while len(game_artifacts) != 0:
-        pass
+        current_artifact = game_artifacts.pop() # way to set variable for all class objects, even after they have been initiliazed?
+        # Players bid on current artifact
+        for p in players:
+            p.bid()
+        # Decide who won
+        # create a dictionary with key "player's bid" to value "player object"
+        bids = {}
+        for p in players:
+            bids[p.bid] = p
+        # get the player object of the player with the highest bid
+        auction_winner = leaderboard[[p_one.score, p_two.score, p_three.score].sort()[-1]]
+        auction_winner.add_artifact(current_artifact)
+        
 
     # create a dictionary with key "player's score" to value "player object"
     leaderboard = {}
     for p in players:
-        leaderboard[p.cal_score()] = p
-    # get the player object of the player with the highest score
-    winner = leaderboard[[p_one.cal_score(), p_two.cal_score(), p_three.cal_score()].sort()[-1]]
-    return winner
+        leaderboard[p.score] = p
+    # get the player object of the player with the highest score (doesn't account for ties currently, but are ties possible?)
+    game_winner = leaderboard[[p_one.score, p_two.score, p_three.score].sort()[-1]]
+    return game_winner
